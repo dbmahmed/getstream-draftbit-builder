5dc773ecb51891aca0cd7c5e85e70758c7ccb9d8
dbmahmed <113385495+dbmahmed@users.noreply.github.com>
2022-11-28 21:26:47 +0600

    set tokens on screen load

--- screens/ChannelListScreen.js
+++ screens/ChannelListScreen.js
@@ -1,12 +1,46 @@
 import React from 'react';
+import * as SportsbettingAPIAuthEndpointsApi from '../apis/SportsbettingAPIAuthEndpointsApi.js';
+import * as GlobalVariables from '../config/GlobalVariableContext';
 import * as ChannelList from '../custom-files/ChannelList.js';
 import * as CustomCode from '../custom-files/CustomCode.js';
 import * as getStreamChatWrapper from '../custom-files/getStreamChatWrapper.js';
 import * as Utils from '../utils';
 import { ScreenContainer } from '@draftbit/ui';
+import { useIsFocused } from '@react-navigation/native';
 import { StyleSheet, View } from 'react-native';
 
 const ChannelListScreen = props => {
+  const Constants = GlobalVariables.useValues();
+  const Variables = Constants;
+  const setGlobalVariableValue = GlobalVariables.useSetValue();
+
+  const isFocused = useIsFocused();
+  React.useEffect(() => {
+    const handler = async () => {
+      try {
+        if (!isFocused) {
+          return;
+        }
+        setGlobalVariableValue({
+          key: 'USER',
+          value: { id: 'ae23dc79-3690-48b1-8bcd-de7d4aa21274' },
+        });
+        const userToken =
+          await SportsbettingAPIAuthEndpointsApi.getGetstreamTokenGET(
+            Constants,
+            { internalId: 'ae23dc79-3690-48b1-8bcd-de7d4aa21274' }
+          );
+        setGlobalVariableValue({
+          key: 'GS_USER_TOKEN',
+          value: userToken,
+        });
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    handler();
+  }, [isFocused]);
+
   return (
     <ScreenContainer hasSafeArea={false} scrollable={false}>
       <Utils.CustomCodeErrorBoundary>
