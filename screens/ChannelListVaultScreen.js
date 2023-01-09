6f044763eaa4f3cf726650ddecbd6195052ac95c
dbmahmed <113385495+dbmahmed@users.noreply.github.com>
2023-01-10 01:15:02 +0600

    fixed screen variables

--- screens/ChannelListVaultScreen.js
+++ screens/ChannelListVaultScreen.js
@@ -5,6 +5,7 @@ import * as CustomCode from '../custom-files/CustomCode.js';
 import * as getStreamChatWrapper from '../custom-files/getStreamChatWrapper.js';
 import * as Utils from '../utils';
 import { ScreenContainer, withTheme } from '@draftbit/ui';
+import { useIsFocused } from '@react-navigation/native';
 import { StyleSheet, View } from 'react-native';
 
 const ChannelListVaultScreen = props => {
@@ -15,7 +16,28 @@ const ChannelListVaultScreen = props => {
     return `token ${token}`;
   };
 
+  const setInitialFilters = () => {
+    setMemoizedFilters({
+      members: { $in: ['843048b9-4ed3-4689-9b84-57d6355af535'] },
+      type: 'messaging',
+    });
+  };
+
   const { theme } = props;
+
+  const isFocused = useIsFocused();
+  React.useEffect(() => {
+    try {
+      if (!isFocused) {
+        return;
+      }
+      setInitialFilters();
+    } catch (err) {
+      console.error(err);
+    }
+  }, [isFocused]);
+
+  const [memoizedFilters, setMemoizedFilters] = React.useState({});
 
   return (
     <ScreenContainer hasSafeArea={false} scrollable={false}>
