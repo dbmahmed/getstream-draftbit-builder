5ab41ff085ff2ff7167635e66078d5a0ca7ed0cd
dbmahmed <113385495+dbmahmed@users.noreply.github.com>
2023-01-10 01:01:53 +0600

    fixed the test screen

--- screens/ChannelListVaultScreen.js
+++ screens/ChannelListVaultScreen.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import * as SportsbettingAPIAuthEndpointsApi from '../apis/SportsbettingAPIAuthEndpointsApi.js';
 import * as GlobalVariables from '../config/GlobalVariableContext';
 import * as ChannelList from '../custom-files/ChannelList.js';
 import * as CustomCode from '../custom-files/CustomCode.js';
 import * as getStreamChatWrapper from '../custom-files/getStreamChatWrapper.js';
 import * as Utils from '../utils';
 import { ScreenContainer, withTheme } from '@draftbit/ui';
-import { useIsFocused } from '@react-navigation/native';
 import { StyleSheet, View } from 'react-native';
 
 const ChannelListVaultScreen = props => {
   const Constants = GlobalVariables.useValues();
   const Variables = Constants;
-  const setGlobalVariableValue = GlobalVariables.useSetValue();
 
   const logToken = token => {
     return `token ${token}`;
@@ -20,44 +17,19 @@ const ChannelListVaultScreen = props => {
 
   const { theme } = props;
 
-  const isFocused = useIsFocused();
-  React.useEffect(() => {
-    const handler = async () => {
-      try {
-        if (!isFocused) {
-          return;
-        }
-        setGlobalVariableValue({
-          key: 'USER',
-          value: { id: 'ae23dc79-3690-48b1-8bcd-de7d4aa21274' },
-        });
-        const userToken =
-          await SportsbettingAPIAuthEndpointsApi.getGetstreamTokenGET(
-            Constants,
-            { internalId: 'ae23dc79-3690-48b1-8bcd-de7d4aa21274' }
-          );
-        console.log(logToken(userToken));
-        setGlobalVariableValue({
-          key: 'GS_USER_TOKEN',
-          value: userToken,
-        });
-      } catch (err) {
-        console.error(err);
-      }
-    };
-    handler();
-  }, [isFocused]);
-
   return (
     <ScreenContainer hasSafeArea={false} scrollable={false}>
       <Utils.CustomCodeErrorBoundary>
-        <getStreamChatWrapper.GetStreamChatProvider>
+        <getStreamChatWrapper.GetStreamChatProvider1>
           <View style={styles(theme).View2200bac7}>
             <Utils.CustomCodeErrorBoundary>
-              <getStreamChatWrapper.GetStreamChatProvider1></getStreamChatWrapper.GetStreamChatProvider1>
+              <ChannelList.ChannelListMod
+                navigation={props.navigation}
+                filter={memoizedFilters}
+              />
             </Utils.CustomCodeErrorBoundary>
           </View>
-        </getStreamChatWrapper.GetStreamChatProvider>
+        </getStreamChatWrapper.GetStreamChatProvider1>
       </Utils.CustomCodeErrorBoundary>
     </ScreenContainer>
   );
